a87fa07ed9dab54ae9f0d738515546f3ac39d144
Lzu Tao <taolzu@gmail.com>
2019-03-16 02:37:16 +0700

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,28 @@ fn main() {
 
 fn arg_parse<'a>() -> ArgMatches<'a> {
     App::new("fbvideo")
-            .version("0.2.0")
-            .about("Get video URLs from Facebook URL.")
-            .author("Lzu Tao")
-            .arg(
-                Arg::with_name("hd")
-                    .help("Get HD quality video URL")
-                    .long("hd")
-                    .conflicts_with("sd"),
-            )
-            .arg(
-                Arg::with_name("sd")
-                    .help("Get SD quality video URL")
-                    .long("sd")
-                    .conflicts_with("hd"),
-            )
-            .arg(
-                Arg::with_name("URL")
-                    .help("List of URLs to get video link")
-                    .multiple(true) // This flag should allow multiple
-                    .required(true), // By default this argument MUST be present
-            )
-            .get_matches()
+        .version("0.2.0")
+        .about("Get video URLs from Facebook URL.")
+        .author("Lzu Tao")
+        .arg(
+            Arg::with_name("hd")
+                .help("Get HD quality video URL")
+                .long("hd")
+                .conflicts_with("sd"),
+        )
+        .arg(
+            Arg::with_name("sd")
+                .help("Get SD quality video URL")
+                .long("sd")
+                .conflicts_with("hd"),
+        )
+        .arg(
+            Arg::with_name("URL")
+                .help("List of URLs to get video link")
+                .multiple(true) // This flag should allow multiple
+                .required(true), // By default this argument MUST be present
+        )
+        .get_matches()
 }
 
 fn run() {
