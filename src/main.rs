e0445af9fa8a6cb1d8a6c4657941196fb028177d
Lzu Tao <taolzu@gmail.com>
2019-03-15 22:11:54 +0700

    Update `fbvideo` to 0.3.0. Bump `fburl` to 0.2.0

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,38 @@
-use clap::{App, Arg};
+use clap::{App, Arg, ArgMatches};
 use fbvideo::{FbVideo, Quality};
 
 fn main() {
-    let mut lists = run();
-    for fb in lists.iter_mut() {
-        match fb.get_video_url() {
-            Ok(url) => println!("{}", url),
-            Err(e) => eprintln!("{:#?}", e),
-        }
-    }
+    run();
 }
 
-fn run() -> Vec<FbVideo> {
-    let matches = App::new("fbvideo")
-        .version("0.1")
-        .about("Get video URLs from Facebook URL.")
-        .author("Lzu Tao")
-        .arg(
-            Arg::with_name("hd")
-                .help("Get HD quality video URL")
-                .long("hd")
-                .conflicts_with("sd"),
-        )
-        .arg(
-            Arg::with_name("sd")
-                .help("Get SD quality video URL")
-                .long("sd")
-                .conflicts_with("hd"),
-        )
-        .arg(
-            Arg::with_name("URL")
-                .help("List of URLs to get video link")
-                .multiple(true) // This flag should allow multiple
-                .required(true), // By default this argument MUST be present
-        )
-        .get_matches();
+fn arg_parse<'a>() -> ArgMatches<'a> {
+    App::new("fbvideo")
+            .version("0.2.0")
+            .about("Get video URLs from Facebook URL.")
+            .author("Lzu Tao")
+            .arg(
+                Arg::with_name("hd")
+                    .help("Get HD quality video URL")
+                    .long("hd")
+                    .conflicts_with("sd"),
+            )
+            .arg(
+                Arg::with_name("sd")
+                    .help("Get SD quality video URL")
+                    .long("sd")
+                    .conflicts_with("hd"),
+            )
+            .arg(
+                Arg::with_name("URL")
+                    .help("List of URLs to get video link")
+                    .multiple(true) // This flag should allow multiple
+                    .required(true), // By default this argument MUST be present
+            )
+            .get_matches()
+}
+
+fn run() {
+    let matches = arg_parse();
 
     let quality = if matches.is_present("sd") {
         Quality::Sd
@@ -42,15 +40,14 @@ fn run() -> Vec<FbVideo> {
         Quality::Hd
     };
 
-    let mut vecs: Vec<FbVideo> = Vec::new();
-
     // We can also get the values for those arguments
     if let Some(urls) = matches.values_of("URL") {
         for url in urls {
-            let fb = FbVideo::new(url, quality);
-            vecs.push(fb);
+            let mut fb = FbVideo::new(url, quality);
+            match fb.get_video_url() {
+                Ok(url) => println!("{}", url),
+                Err(e) => eprintln!("{:#?}", e),
+            }
         }
     }
-
-    vecs
 }
