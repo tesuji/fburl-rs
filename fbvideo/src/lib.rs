1722281aecce66b4286abbff85e1d97cc7149b28
Lzu Tao <taolzu@gmail.com>
2019-10-30 16:35:41 +0700

    doc: remove needless main in doctest

--- fbvideo/src/lib.rs
+++ fbvideo/src/lib.rs
@@ -12,15 +12,13 @@
 //! ```rust,no_run
 //! use fbvideo::{FbVideo, Quality};
 //!
-//! fn main() {
-//!     let mut fb = FbVideo::new(
-//!         "https://www.facebook.com/817131355292571/videos/2101344733268123/",
-//!         Quality::Hd,
-//!     );
-//!     match fb.get_video_url() {
-//!         Ok(url) => println!("{:?}", url),
-//!         Err(e) => panic!("{:?}", e),
-//!     }
+//! let mut fb = FbVideo::new(
+//!     "https://www.facebook.com/817131355292571/videos/2101344733268123/",
+//!     Quality::Hd,
+//! );
+//! match fb.get_video_url() {
+//!     Ok(url) => println!("{:?}", url),
+//!     Err(e) => panic!("{:?}", e),
 //! }
 //! ```
 
